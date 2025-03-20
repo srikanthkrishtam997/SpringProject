6bc79bc3f056c16aa7549e319c2f36a60b1521c7
srikanth.krishtam <srikanth.krishtam@HYDPCM627633L.ad.infosys.com>
2025-03-20 12:39:10 +0530

    test333

--- src/com/java/core/streams/FindMaxMinElement.java
+++ src/com/java/core/streams/FindMaxMinElement.java
@@ -9,7 +9,7 @@ public class FindMaxMinElement {
 public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(10, 20, 35, 50, 5, 80, 15);
     int i=numbers.stream().max(Integer::compareTo).orElse(0);
-    
+    //Second Way
     System.out.println(numbers.stream().sorted(Collections.reverseOrder()).skip(0).findFirst().orElse(0));
 }
 }
